76a8eb8d8d2b2d9c53ee449cbc35b9979c43150a
Pranjal Mantri <pranjalmantri@gmail.com>
2025-04-13 22:55:14 +0530

    feat: add Zod for request validation middleware

---
+++ backend/src/middlwares/validateResource.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import { AnyZodObject, ZodError } from "zod";
+
+const validateResource =
+  (schema: AnyZodObject) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.safeParse({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+
+      next();
+    } catch (error: any) {
+      if (error instanceof ZodError) {
+        res.status(400).json({
+          status: "fail",
+          errros: error.errors.map((e) => ({
+            field: e.path.join("."),
+            message: e.message,
+          })),
+        });
+      }
+
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  };
