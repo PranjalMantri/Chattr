d940cc434965376962cc6a96901f2739f7d05380
Pranjal Mantri <pranjalmantri@gmail.com>
2025-04-14 16:22:40 +0530

    feat: add basic routing and page skeleton

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,27 @@
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import {
+  HomePage,
+  SignupPage,
+  LoginPage,
+  ProfilePage,
+  SettingsPage,
+} from "./pages";
+
 function App() {
-  return <div className="text-3xl">Hello world</div>;
+  return (
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/signup" element={<SignupPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/profile" element={<ProfilePage />}></Route>
+        <Route path="/settings" element={<SettingsPage />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,7 @@
+import HomePage from "./HomePage";
+import SignupPage from "./SignupPage";
+import LoginPage from "./LoginPage";
+import ProfilePage from "./ProfilePage";
+import SettingsPage from "./SettingsPage";
+
+export { HomePage, SignupPage, LoginPage, ProfilePage, SettingsPage };
