87803918be5f23d78c79178741a21a7361befed9
Pranjal Mantri <pranjalmantri@gmail.com>
2025-04-14 17:38:48 +0530

    feat: add protected paths

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,17 +7,46 @@ import {
   ProfilePage,
   SettingsPage,
 } from "./pages";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
 
 function App() {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin"></Loader>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/signup" element={<SignupPage />}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/profile" element={<ProfilePage />}></Route>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <LoginPage />}
+        ></Route>
+        <Route
+          path="/signup"
+          element={!authUser ? <SignupPage /> : <HomePage />}
+        ></Route>
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <HomePage />}
+        ></Route>
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <LoginPage />}
+        ></Route>
         <Route path="/settings" element={<SettingsPage />}></Route>
       </Routes>
     </div>
--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -1,6 +1,32 @@
 import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
 
-export const useAuthStore = create(() => ({
-  userAuth: null,
+interface AuthState {
+  authUser: any;
+  isSigningUp: boolean;
+  isLoggingIn: boolean;
+  isUpdatingProfile: boolean;
+  isCheckingAuth: boolean;
+  checkAuth: () => Promise<void>;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+
   isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("Error in checking auth: ", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
 }));
