24c04a1b80797b08b8c8c820b8792e8b51518d81
Pranjal Mantri <pranjalmantri@gmail.com>
2025-10-06 21:56:36 +0530

    fix: update SameSite attribute for cookie to 'none' for cross-site requests

--- backend/src/lib/utils.ts
+++ backend/src/lib/utils.ts
@@ -8,7 +8,7 @@ export const generateToken = (userId: string, res: Response) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    sameSite: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
     secure: process.env.NODE_ENV !== "development",
   });
