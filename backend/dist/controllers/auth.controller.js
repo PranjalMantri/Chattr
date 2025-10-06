f21837cf5aa025279b01f95a9ab92168a2ef9ba8
Pranjal Mantri <pranjalmantri@gmail.com>
2025-10-06 15:38:25 +0530

    feat: remove unused backend files and update package.json for build and start scripts

--- backend/dist/controllers/auth.controller.js
+++
@@ -1,9 +0,0 @@
-export const signup = (req, res) => {
-    res.send("Signup");
-};
-export const login = (req, res) => {
-    res.send("Signup");
-};
-export const logout = (req, res) => {
-    res.send("Signup");
-};
