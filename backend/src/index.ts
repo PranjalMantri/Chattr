acb4f37c82fc942d90ed8758e41222ce76f44420
Pranjal Mantri <pranjalmantri@gmail.com>
2025-10-06 20:23:13 +0530

    fix: update CORS origin to production URL for API and socket server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chattr-hazel.vercel.app/",
     credentials: true,
   })
 );
